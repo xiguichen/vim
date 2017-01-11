903a924ec9035e4aa9250faf679a8f0d196c557e
xiguichen <chenxigui_192930@163.com>
2017-01-11 13:14:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# init and update submodule
+git submodule init
+git submodule update
+
 # install jsbeautify
 cd ~/.vim/bundle/vim-jsbeautify && git submodule update --init --recursive
+
+# install youcompleteme
+cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive && python install.py --clang-completer --tern-completer
