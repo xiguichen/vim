70d72d8d8241c268104660522496d8db0164a21a
Xigui Chen <xigui.chen@ericsson.com>
2018-01-10 08:39:00 +0100

    made some update about the install script

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# init and update submodule
-git submodule init
-git submodule update
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
 # install jsbeautify
 cd ~/.vim/bundle/vim-jsbeautify && git submodule update --init --recursive
 
 # install youcompleteme
-cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive && python install.py --clang-completer --tern-completer
+cd ~/.vim/bundle/YouCompleteMe/ && git submodule update --init --recursive && python install.py --tern-completer
 
 # install meteor tern
+cd ~/.vim/bundle/tern_for_vim/ && npm install
 cd ~/.vim && cp -f post_fix/meteor.json bundle/tern_for_vim/node_modules/tern/defs/meteor.json
+echo "Please set your environment variable 'TERM' to 'screen-256color' in order to make the color scheme work"
